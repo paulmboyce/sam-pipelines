f311893988fbba93de00de0cf82896439e5a5726
paul-ecswkshp1 <ecsworkshop1@gmail.com>
2023-05-15 20:25:21 +0000

    cicd(integration test): add integration/test.sh

---
+++ hello-world/tests/integration/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+STACK_NAME=sam-workshop-dev-stage
+
+DEV_ENDPOINT=$(aws cloudformation describe-stacks --stack-name $STACK_NAME  | jq -r '.Stacks[].Outputs[].OutputValue | select(startswith("https://"))')
+
+echo "Dev endpoint: $DEV_ENDPOINT"
+curl -s $DEV_ENDPOINT
+
+
+RESULT=$(curl -H "Accept: application/json"  $DEV_ENDPOINT | jq '.message' )
+
+EXPECT_CONTAINS=*"hello world"*
+
+if [[ $RESULT = $EXPECT_CONTAINS ]]; then
+    echo "test PASS"    
+    exit 0
+  else     
+    echo "test FAIL: expected $EXPECT_CONTAINS"    
+    exit 1
+  fi
