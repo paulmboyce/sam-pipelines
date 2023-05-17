11b1562aa45d4f7f0832f4045ac79e6f5c264b82
paul-ecswkshp1 <ecsworkshop1@gmail.com>
2023-05-17 18:09:08 +0000

    refactor(extract method): updateDynamoDBTable

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -5,41 +5,46 @@ import { v4 as uuidv4 } from 'uuid';
 const dynamoDB = new DynamoDBClient({ region: process.env.AWS_REGION });
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    let response: APIGatewayProxyResult;
-
-    //throw new Error('This will cause a deployment rollback');
-
     try {
         const messageId = uuidv4();
         const currentDate = new Date();
-        const messageItem = {
-            messageId: { S: messageId },
-            message: { S: 'HelloWorldFunction executed in ' + process.env.AWS_REGION },
-            timestamp: { S: currentDate.toISOString() },
-        };
 
-        const params = {
-            TableName: 'message-table',
-            Item: messageItem,
-        };
+        await updateDynamoDBTable(messageId, currentDate);
 
-        await dynamoDB.send(new PutItemCommand(params));
-
-        response = {
+        const response: APIGatewayProxyResult = {
             statusCode: 200,
             body: JSON.stringify({
                 message: 'hello paul',
                 deployment: 'canary: Canary10Percent5Minutes',
             }),
         };
-    } catch (err) {
-        console.error(err);
-        response = {
+
+        return response;
+    } catch (error) {
+        console.error(error);
+
+        const errorResponse: APIGatewayProxyResult = {
             statusCode: 500,
             body: JSON.stringify({
-                message: err instanceof Error ? err.message : 'some error happened',
+                message: error instanceof Error ? error.message : 'some error happened',
             }),
         };
+
+        return errorResponse;
     }
-    return response;
+};
+
+const updateDynamoDBTable = async (messageId: string, currentDate: Date): Promise<void> => {
+    const messageItem = {
+        messageId: { S: messageId },
+        message: { S: 'HelloWorldFunction executed in ' + process.env.AWS_REGION },
+        timestamp: { S: currentDate.toISOString() },
+    };
+
+    const params = {
+        TableName: 'message-table',
+        Item: messageItem,
+    };
+
+    await dynamoDB.send(new PutItemCommand(params));
 };
