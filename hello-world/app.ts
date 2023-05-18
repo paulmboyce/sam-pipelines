b88829b9fc55eb1abf1d4ee0a88ba446c7816d34
paul-ecswkshp1 <ecsworkshop1@gmail.com>
2023-05-18 13:37:22 +0000

    feature(lambda):add logging as a reason for canary

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,6 +1,8 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log('running...');
+
     return generateResponse(200, {
         message: 'hello paul',
     });
