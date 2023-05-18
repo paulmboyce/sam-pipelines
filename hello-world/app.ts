9a5ed1861c5239c941f84897d44e8df6dad77947
paul-ecswkshp1 <ecsworkshop1@gmail.com>
2023-05-18 11:26:10 +0000

    refactor(lambda):remove surplus err handling

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,21 +1,9 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
-        const response = generateResponse(200, {
-            message: 'hello paul',
-        });
-
-        return response;
-    } catch (error) {
-        console.error(error);
-
-        const errorResponse = generateResponse(500, {
-            message: error instanceof Error ? error.message : 'some error happened',
-        });
-
-        return errorResponse;
-    }
+    return generateResponse(200, {
+        message: 'hello paul',
+    });
 };
 
 const generateResponse = (statusCode: number, body: object): APIGatewayProxyResult => {
